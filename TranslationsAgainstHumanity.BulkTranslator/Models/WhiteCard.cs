7aaac79d04d42d242b21355cba7d4cc9db2155e2
janex-PL <jankesghost@gmail.com>
2019-10-31 12:51:31 +0100

    Changed field to more flexible status code

--- TranslationsAgainstHumanity.BulkTranslator/Models/WhiteCard.cs
+++ TranslationsAgainstHumanity.BulkTranslator/Models/WhiteCard.cs
@@ -1,27 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace TranslationsAgainstHumanity.BulkTranslator.Models
+﻿namespace TranslationsAgainstHumanity.BulkTranslator.Models
 {
+	/// <summary>
+	/// TAH model of white card
+	/// </summary>
 	public class WhiteCard
 	{
+		#region Fields
+
+		/// <summary>
+		/// Contains original card text
+		/// </summary>
 		public string OriginalText { get; set; }
+
+		/// <summary>
+		/// Contains translated card text
+		/// </summary>
 		public string TranslatedText { get; set; }
 
-		public bool IsCardReady { get; set; }
+		/// <summary>
+		/// Describes completion status of translation
+		/// 
+		/// 0 = Translation not ready to be published
+		/// 1 = Translation ready to publish
+		/// -1 = Discard card
+		/// </summary>
+		public int TranslationStatus { get; set; }
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
 		public WhiteCard()
-		{
+		{ 
 			OriginalText = string.Empty;
 			TranslatedText = string.Empty;
-			IsCardReady = false;
+			TranslationStatus = 0;
 		}
+
+		/// <summary>
+		/// Constructor with original text
+		/// </summary>
+		/// <param name="originalText">Original text from card</param>
 		public WhiteCard(string originalText)
 		{
 			OriginalText = originalText;
 			TranslatedText = string.Empty;
-			IsCardReady = false;
+			TranslationStatus = 0;
 		}
+
+		#endregion
 	}
 }
 
